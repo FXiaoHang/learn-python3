63c3a86ca9a64f78f788075bbcd7eb84b0b95e8a
FXiaoHang <1606930741@qq.com>
2023-01-15 21:45:43 +0800

    一些修改

--- samples/basic/do_for.py
+++ samples/basic/do_for.py
@@ -2,10 +2,15 @@
 # -*- coding: utf-8 -*-
 
 # 打印list:
-names = ['Michael', 'Bob', 'Tracy']
-for name in names:
-    print(name)
+# names = ['Michael', 'Bob', 'Tracy']
+# for name in names:
+#     print(name)
 
 # 打印数字 0 - 9
-for x in range(10):
-    print(x)
+# for x in range(10):
+#     print(x)
+
+sum1 = 0
+for x in range(100):
+    sum1 = sum1 + x
+print(sum1)
